b1cd415416827e62e72cf04b18a70271b42edd10
Saad Shakhshir <shakhshir@gmail.com>
2011-08-05 18:48:20 +0400

    Completed signin/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController 
+  
   def new
     @title = "Sign in"
   end
@@ -14,6 +15,11 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
     end
+  end
+  
+  def destroy
+    sign_out
+    redirect_to root_path
   end
 
 end
