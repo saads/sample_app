50d575550863b33489b37e06400618863397ffa3
Saad Shakhshir <shakhshir@gmail.com>
2011-08-08 22:04:42 +0400

    Can now destroy users (as admins)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -148,7 +148,27 @@ describe User do
     
   end
   
+  describe 'admin attribute' do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it 'should response to admin' do
+      @user.should respond_to(:admin)
+    end
+    
+    it 'should not be an admin by default' do
+      @user.should_not be_admin
+    end
+    
+    it 'should be convertible into an admin' do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
 end
+
 
 
 
@@ -163,5 +183,6 @@ end
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
