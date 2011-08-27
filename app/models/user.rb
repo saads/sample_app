dedff251e40defddae096fd859bb06c00a279734
Saad Shakhshir <shakhshir@gmail.com>
2011-08-27 17:38:37 +0400

    User relationships (following/followers)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :dependent => :destroy,
+                            :foreign_key => "follower_id"
+  has_many :reverse_relationships, :dependent => :destroy,
+                                    :foreign_key => "followed_id",
+                                    :class_name => "Relationship"
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -25,6 +32,19 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where("user_id = ?", self.id)
   end
+  
+  def following?(followed)
+    self.relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    self.relationships.create!(:followed_id => followed.id)    
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed.id).destroy
+  end
+    
   
   def User.authenticate(email, submitted_password)
     user = User.find_by_email(email)
